09f33a1555bf1db8135d67e5e783a9d81e674a4f
Lacey <lacey.griffith@g.austincc.edu>
2021-06-28 15:17:49 -0500

    set up user typeDef and resolver

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,37 @@ const {gql} = require('apollo-server-express');
 
 // create the typeDefs
 const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        friendCount: Int
+        thoughts: [Thought]
+        friends: [User]
+    }
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
     type Query {
-        helloWorld: String
+        users: [User]
+        user(username: String!): User
+        thoughts(username: String): [Thought]
+        thought(_id: ID!): Thought
     }
 `;
+// above: ID is a unique identifying string
+// ! indicates that the parameter data MUST exist to retrieve data
 
 // export the type defs
 module.exports = typeDefs;
